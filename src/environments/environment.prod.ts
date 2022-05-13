13d9f0db92774ba003963a6af37e8632970b4b89
Marco Antonio De Araujo Junior <59663846+Marcoant007@users.noreply.github.com>
2022-05-13 16:12:32 -0300

    alteração de env de prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api_quarkus_fiscal: 'http://localhost:8083/',
+  api_quarkus_fiscal: 'http://dfe.yooga.com.br/',
   api_prod_fiscal: 'http://dfe.yooga.com.br/',
   retornoNota : 'https://api2.yooga.com.br/fiscalservice/reenviar/',
   usuario: 'admin',
