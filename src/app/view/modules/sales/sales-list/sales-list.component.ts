508f020da9ecc353f6290cd195591cbc7659e3ba
Marco Antonio De Araujo Junior <59663846+Marcoant007@users.noreply.github.com>
2022-05-16 10:39:10 -0300

    adicionando verificação de status de error

--- src/app/view/modules/sales/sales-list/sales-list.component.ts
+++ src/app/view/modules/sales/sales-list/sales-list.component.ts
@@ -9,22 +9,22 @@ import { SalesService } from '../sales.service';
 @Component({
   selector: 'app-sales-list',
   templateUrl: './sales-list.component.html',
-  styleUrls: ['./sales-list.component.scss']
+  styleUrls: ['./sales-list.component.scss'],
 })
 export class SalesListComponent implements OnInit {
-
   vendas: Venda[];
   key: string = 'nome';
   reverse: boolean = false;
   isClicked: boolean = false;
   transmitidas = 0;
 
-
-  constructor(private salesService: SalesService,
+  constructor(
+    private salesService: SalesService,
     public router: Router,
     private http: HttpClient,
     private clipboardApi: ClipboardService,
-    private activatedRoute: ActivatedRoute) { }
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   async ngOnInit() {
     await this.loadQuarkusList();
@@ -34,33 +34,40 @@ export class SalesListComponent implements OnInit {
     this.vendas = await this.salesService.findVendasPendentes();
   }
 
-
-  sort(key:any) {
-      this.key = key;
-      this.reverse = !this.reverse;
+  sort(key: any) {
+    this.key = key;
+    this.reverse = !this.reverse;
   }
 
   async copyText(codigo: any) {
-    this.clipboardApi.copyFromContent(codigo)
+    this.clipboardApi.copyFromContent(codigo);
     this.salesService.showMessageCopy(`Código ${codigo} copiado`);
   }
 
-
-  async reenviar(codigo:number){
+  async reenviar(codigo: number) {
     this.isClicked = true;
-    this.salesService.reenviarNotasPendentes(codigo).then(response => {
-      if(response.notaEvento.cStat === 150 || response.notaEvento.cStat === 100){
-        this.salesService.showMessage(`${response.notaEvento.cStat} - ${response.notaEvento.xMotivo} código: ${codigo} `);
-        this.vendas.forEach((element, index) => {
-          if(element.codigo === codigo){
-            this.vendas.splice(index, 1);
-            this.transmitidas++
-          };
-          
-        });
-      }else {
-        this.salesService.showMessageError(`${response.notaEvento.cStat} - ${response.notaEvento.xMotivo} código: ${codigo} `);
-      }
-    });
+
+      this.salesService.reenviarNotasPendentes(codigo).then((response) => {
+
+        if(response.status === 500) {
+          return this.salesService.showMessageError(response.message);
+        }
+
+        if (response.notaEvento.cStat === 150 || response.notaEvento.cStat === 100) {
+          this.salesService.showMessage(`${response.notaEvento.cStat} - ${response.notaEvento.xMotivo} código: ${codigo} `);
+
+          this.vendas.forEach((element, index) => {
+            if (element.codigo === codigo) {
+              this.vendas.splice(index, 1);
+              this.transmitidas++;
+            }
+          });
+
+        } else {
+          this.salesService.showMessageError(`${response.notaEvento.cStat} - ${response.notaEvento.xMotivo} código: ${codigo} `);
+        }
+      }).catch(error => {
+        return this.salesService.showMessageError(error.error.message);
+      })
   }
 }
